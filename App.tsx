d2fde8e54a52c1744ce090413e205097b878045b
sobanarshad85 <sobanarshad85@gmail.com>
2023-03-28 00:11:54 +0500

    store persistent implemented.

--- App.tsx
+++ App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import store from './src/store';
 import Screens from './src/navigation';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
 
 function App() {
+  let persistor = persistStore(store);
   return (
     <Provider store={store}>
-      <Screens />
+      <PersistGate loading={null} persistor={persistor}>
+        <Screens />
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,32 @@
+import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { pokemonApi } from './api/pokemonApi';
 import pokemonReducer from './pokemonSlice';
 import logger from 'redux-logger';
+import { persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
+const reducers = combineReducers({
+  pokemon: pokemonReducer,
+  [pokemonApi.reducerPath]: pokemonApi.reducer,
+
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['pokemon'],
+  stateReconciler: autoMergeLevel2,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    pokemon: pokemonReducer,
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(pokemonApi.middleware, logger),
 });
-
 setupListeners(store.dispatch);
-
-export type RootState = ReturnType<typeof store.getState>;
 export default store;
